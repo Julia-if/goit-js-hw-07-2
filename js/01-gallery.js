b58210a2c94ddd11897858aebe3c64a3e5b41994
Julia-if <juliaif18@gmail.com>
2022-01-02 00:00:00 +0200

    task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+function createGalery(galleryItems) {
+    return galleryItems.map(({ original, preview, description }) => {
+        return `<div class = "gallery__item">
+            <a class = "gallery__link" href = "${original}">
+            <img class = "gallery__image" src = "${preview}"
+        data - source = "${original}"
+        alt = "${description}"/>
+            </a> </div>`;
+    }).join('');
+};
+const galleryContainerRef = document.querySelector('.gallery');
+const cardsMarkUp = createGalery(galleryItems);
+galleryContainerRef.insertAdjacentHTML("beforeend", cardsMarkUp);
+galleryContainerRef.addEventListener("click", onCardsClick);
+
+function onCardsClick(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
+        return;
+    };
+    galleryItems.map((item) => {
+        if (item.original !== e.target.dataset.source) {
+            const instance = basicLightbox.create(`<img src=${item.original} width='800' height='600'>`);
+            instance.show();
+        };
+    });
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,16 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+function createGalery(galleryItems) {
+    return galleryItems.map(({ original, preview, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    }).join('');
+};
+const galleryContainerRef = document.querySelector('.gallery');
+const cardsMarkUp = createGalery(galleryItems);
+galleryContainerRef.insertAdjacentHTML("beforeend", cardsMarkUp);
+
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionType: 'attr', captionDelay: 250, });
